258540a1e7cbb7384bac591417ab32779f3021f3
beard <joshuabeard92@gmail.com>
2019-06-02 20:41:09 -0400

    updates for clean node fixes

--- vimwiki_link_network.py
+++ vimwiki_link_network.py
@@ -15,11 +15,9 @@ parser.add_argument('entry_point',
                     type=str,
                     nargs=1,
                     metavar='EntryPoint',
-                    action='store_constant',
                     help='file name of entry point for graph traversal')
 parser.add_argument('graph_type',
                     type=str,
-                    action='store_constant',
                     metavar='NetworkXGraphType',
                     help='Type of graph to display')
 parser.add_argument('--show',
@@ -93,26 +91,44 @@ class VimwikiGraph(nx.DiGraph):
         return re.sub(self.root_dir, '', node_path)
 
     def organize(self):
-        nlist = [['/index']]
+        nlist = [['index']]
         out_degs = np.array([d for n, d in self.out_degree()])
         in_degs = np.array([d for n, d in self.in_degree()])
         nodes = np.array([n for n, d in self.in_degree()])
-        out_degs = out_degs[np.where(nodes != '/index')]
-        in_degs = in_degs[np.where(nodes != '/index')]
-        nodes = nodes[np.where(nodes != '/index')]
+        out_degs = out_degs[np.where(nodes != 'index')]
+        in_degs = in_degs[np.where(nodes != 'index')]
+        nodes = nodes[np.where(nodes != 'index')]
         ratio = out_degs / in_degs
         mean_rat = ratio.mean()
         std_rat = np.std(ratio)
-        nlist.append(nodes[np.where(ratio > mean_rat + std_rat)])
-        nlist.append(nodes[np.where(ratio <= mean_rat + std_rat * (ratio > mean_rat))])
-        nlist.append(nodes[np.where(ratio <= mean_rat)])
+        more_nodes = nodes[np.where(ratio > mean_rat + std_rat)]
+        if len(more_nodes):
+            nlist.append(more_nodes)
+        more_nodes = nodes[np.where(ratio <= mean_rat + std_rat * (ratio > mean_rat))]
+        if len(more_nodes):
+            nlist.append(more_nodes)
+        more_nodes = nodes[np.where(ratio <= mean_rat)]
+        if len(more_nodes):
+            nlist.append(more_nodes)
         self.nlist = nlist
         self._out_degs = out_degs
         self._in_degs = in_degs
         self._nodes = nodes
         self._ratio = ratio
 
-    def visit_all(self, expand_edges=False):
+    def clean_nodes(self):
+        """ Cleans up nodes so that none of the directory structure is
+            maintained - this makes for prettier graphs, though you lose some
+            information in the process
+        """
+        def clean_node_pair(edge):
+            split_edge = [e.split(os.path.sep) for e in edge]
+            split_edge = [edge[-1] for edge in split_edge]
+            return split_edge
+
+        self.simple_edges = [clean_node_pair(edge) for edge in self.simple_edges]
+
+    def visit_all(self, expand_edges=False, keep_dir_structure=False):
         if self._db:
             import ipdb
             ipdb.set_trace()
@@ -127,13 +143,24 @@ class VimwikiGraph(nx.DiGraph):
         if expand_edges and not self.expanded:
             self.expand_all_edges()
 
+        if not keep_dir_structure:
+            self.clean_nodes()
+
         self.add_edges_from(self.simple_edges)
         self.organize()
 
     def expand_edges(self, edge):
-        """Here, edge should be a simple edge (that is, no root_dir)"""
-        split_edge = [n.split(os.path.sep) for n in edge]
-        split_edge = [i if len(i[0]) > 0 else i[1:] for i in split_edge]
+        """ Expands all edges to create intermediate nodes for directories
+            This may be useful if you're just trying to create a graph of the
+            directory structure, but doesn't really help you learn about the
+            knowledge graph. I may abandon this because it's not particularly
+            helpful in understanding the Vimwiki knowledge graph.
+
+            Here, edge should be a simple edge (that is, no root_dir)
+        """
+        split_edge = [e.split(os.path.sep) for e in edge]
+        split_edge = [loc if len(loc[0]) > 0 else loc[1:] for loc in split_edge]
+        split_edge[0] = [split_edge[0][-1]]
         nodes = list(chain(*split_edge))
         expanded_edge = [[nodes[i], nodes[i + 1]] for i in range(len(nodes) - 1)]
         return expanded_edge
@@ -149,6 +176,8 @@ class VimwikiGraph(nx.DiGraph):
             nx.draw(self, with_labels=True)
         elif draw_type.lower() == 'shell':
             nx.draw_shell(self, with_labels=True, nlist=self.nlist)
+        elif draw_type.lower() == 'planar':
+            nx.draw_planar(self, with_labels=True, nlist=self.nlist)
         elif draw_type.lower() == 'circular':
             nx.draw_circular(self, with_labels=True)
         elif draw_type.lower() == 'spectral':
@@ -179,6 +208,9 @@ class VimwikiGraph(nx.DiGraph):
         # Strip off brackets and disregard links to self
         links = [s.strip('[[').strip(']]') for s in links if not s[2] == '#']
 
+        # Remove all subreferences
+        links = [re.sub('#.*', '', s) for s in links]  # NOQA
+
         # Strip off link aliases
         links = [re.sub('\|.*', '', s) for s in links]  # NOQA
 
@@ -192,5 +224,5 @@ if __name__ == "__main__":
     graph = VimwikiGraph(ep)
     import ipdb
     with ipdb.launch_ipdb_on_exception():
-        graph.visit_all()
+        graph.visit_all(expand_edges=False, keep_dir_structure=False)
         graph.plot_nx_graph(draw_type=args.graph_type, save=args.save, show=args.show)
